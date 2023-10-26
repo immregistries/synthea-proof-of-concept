a8a5b490cca8aaf1180b1366cc059dd395857b12
Francois Merat <Francois.Merat@telecomnancy.eu>
2023-10-26 16:33:30 +0200

    [+] SpringBoot project with Synthea

---
+++ src/main/java/eu/telecomnancy/syntheaproofofconcept/SyntheaProofOfConceptApplication.java
@@ -0,0 +1,13 @@
+package eu.telecomnancy.syntheaproofofconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntheaProofOfConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntheaProofOfConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/telecomnancy/syntheaproofofconcept/SyntheaProofOfConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.telecomnancy.syntheaproofofconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntheaProofOfConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
